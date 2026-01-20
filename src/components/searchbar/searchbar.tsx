fa16948dbc06c76ca54ae9c44657d47cd97c2d2e
Eric Man <youzai0427@gmail.com>
2026-01-20 10:52:51 +0800

    feat: enhance Searchbar component with improved input placeholder text

--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -31,14 +31,34 @@ export default function Searchbar() {
 
   return (
     <fieldset className="fieldset relative">
-      <input
-        value={inputValue}
-        onChange={(e) => handleChange(e)}
-        type="text"
-        className="input w-full"
-        placeholder="What topics do you like?"
-      />
-      <section className="absolute top-full left-0 z-10 flex w-full flex-col divide-y divide-sky-100 rounded-2xl bg-white/30 backdrop-blur-md">
+      <label className="input w-full">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input
+          type="search"
+          value={inputValue}
+          onChange={(e) => handleChange(e)}
+          required
+          placeholder="What topics do you like? ( webdev / c# )"
+          className="w-full"
+        />
+      </label>
+
+      <section className="absolute top-full left-0 z-10 flex w-full flex-col divide-y divide-sky-100 overflow-hidden rounded-lg bg-white/30 backdrop-blur-md">
         {searchList.map((item: string, index: number) => (
           <DifficultyOption key={item} item={item} />
         ))}

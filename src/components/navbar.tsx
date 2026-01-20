ebd198e35acad3baf122b1f90b6eec6fc4259019
Eric Man <youzai0427@gmail.com>
2026-01-20 10:49:28 +0800

    feat(UI): enhance Navbar with FontAwesome icon and improve title styling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,6 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Navbar() {
   return (
     <div className="navbar bg-base-100 shadow-sm">
@@ -5,12 +8,12 @@ export default function Navbar() {
         <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
         <div className="dropdown">
           <label htmlFor="my-drawer-3" className="btn drawer-button lg:hidden">
-            O
+            <FontAwesomeIcon icon={faBars} />
           </label>
         </div>
       </div>
       <div className="navbar-center">
-        <a className="btn btn-ghost text-xl">Programmer Quiz</a>
+        <h1 className="text-xl font-medium text-black">Programmer Quiz</h1>
       </div>
       <div className="navbar-end"></div>
     </div>

5fe5c89dc4e62f4d30116f0d63d3df5e0c293472
Eric Man <youzai0427@gmail.com>
2026-01-20 10:52:10 +0800

    feat: refactor DifficultyOption component and enhance button styling

--- src/components/searchbar/difficultyOption.tsx
+++ src/components/searchbar/difficultyOption.tsx
@@ -1,20 +1,18 @@
-import { useSelectedTopicsStore } from "@/src/store/useSelectedTopicsStore";
-import { useDifficultyModalStore } from "@/src/store/useDifficultyModalStore";
+import { useDifficultyModal } from "@/src/store/useDifficultyModalStore";
 
 interface OptionProps {
   item: string;
 }
 
 export default function DifficultyOption({ item }: OptionProps) {
-  const difficultyModalStore = useDifficultyModalStore();
-
-  const itemHandleClick = () => difficultyModalStore.openModal(item);
+  const { openModal } = useDifficultyModal();
+  const itemHandleClick = () => openModal(item);
 
   return (
     <>
       <button
         onClick={itemHandleClick}
-        className="text-1xl p-4 text-left hover:cursor-pointer hover:bg-white/50"
+        className="text-1xl p-4 text-left font-medium hover:cursor-pointer hover:bg-white/50"
       >
         {item}
       </button>

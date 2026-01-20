c4352412f570a72cf4ee1714d93a8aa7a9910ca1
Eric Man <youzai0427@gmail.com>
2026-01-20 10:51:22 +0800

    feat: refactor SelectedTopic component to improve button functionality and update state management

--- src/components/sidebar/selectedTopic.tsx
+++ src/components/sidebar/selectedTopic.tsx
@@ -1,32 +1,30 @@
 "use client";
 
-import { useSelectedTopicsStore } from "@/src/store/useSelectedTopicsStore";
+import { useQuizOption } from "@/src/store/useSelectedTopicsStore";
 import Topic from "@/src/types/topic";
 
 const color: any = {
-  easy: "badge-success",
-  medium: "badge-warning",
-  hard: "badge-error",
+  easy: "btn-success",
+  medium: "btn-warning",
+  hard: "btn-error",
 };
 
 export default function SelectedTopic({ topic }: { topic: Topic }) {
-  const selectedTopicsStore = useSelectedTopicsStore();
+  const { removeTopic } = useQuizOption();
 
   const handleRemove = () => {
-    selectedTopicsStore.removeTopic(topic);
+    removeTopic(topic);
   };
 
   return (
-    <div
-      className={`badge badge-outline group transition-all ${color[topic.difficulty]}`}
+    <button
+      onClick={() => handleRemove()}
+      className={`btn btn-outline group transition-all duration-75 ${color[topic.difficulty]}`}
     >
-      <h1> {topic.name}</h1>
-      <button
-        onClick={() => handleRemove()}
-        className="cursor-pointer md:hidden md:group-hover:block"
-      >
+      <h1 className="-mr-3 group-hover:mr-0">{topic.name}</h1>
+      <h1 className="invisible transition-all duration-75! group-hover:visible">
         X
-      </button>
-    </div>
+      </h1>
+    </button>
   );
 }

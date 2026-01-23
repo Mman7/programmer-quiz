ce756aecb0410f01b1ab5794bcdb0f472fe6fd73
Eric Man <youzai0427@gmail.com>
2026-01-23 13:38:46 +0800

    refactor: remove SelectDifficultyModal and StartGameModal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
 "use client";
 
-import SelectDifficultyModal from "../components/modal/selectDifficultyModal";
-import StartGameModal from "../components/modal/startModal/startGameModal";
 import Searchbar from "../components/searchbar/searchbar";
 import StartQuizButton from "../components/startQuizButton";
-import { useQuizOption } from "@/src/store/useSelectedTopicsStore";
 
 export default function Home() {
-  const selectedTopicsStore = useQuizOption();
   return (
     <div className="m-auto w-3/4 max-w-2xl">
       <Searchbar />
       <StartQuizButton />
-      <SelectDifficultyModal
-        chooseFn={(topic) => selectedTopicsStore.addTopic(topic)}
-      />
-      <StartGameModal />
     </div>
   );
 }

fd0b614215027e5cee873dc91d58f4d463666e0c
Eric Man <youzai0427@gmail.com>
2026-01-24 06:46:49 +0800

    refactor : reduce code complexity

--- src/components/quizPage/questionOptions.tsx
+++ src/components/quizPage/questionOptions.tsx
@@ -1,16 +1,17 @@
 import { QuizQuestion } from "@/src/types/quizQuestion";
-import { useState } from "react";
 
 const indexOrder: string[] = ["A.", "B.", "C.", "D."];
 
 interface QuestionOptionsProps {
   question: QuizQuestion;
-  answer: string;
+  isSubmit: boolean;
+  answer: string | null;
   selectedAnswer: string;
   setSetselectedAnswer: (option: string) => void;
 }
 
 export default function QuestionOptions({
+  isSubmit,
   question,
   answer,
   selectedAnswer,
@@ -18,26 +19,34 @@ export default function QuestionOptions({
 }: QuestionOptionsProps) {
   const isOptionLessThan3Character: boolean = question.options[0].length < 3;
   const btnBlockStyle = isOptionLessThan3Character && "p-6";
-  const selected = (option: string) =>
-    selectedAnswer === option && "bg-info! text-white! scale-105";
-  const correctAnswer =
-    answer === selectedAnswer && "green-300 text-shadow-gray-700!";
+  const getOptionStyle = (option: string) => {
+    if (!isSubmit)
+      return option === selectedAnswer ? "bg-info! text-white! scale-105" : "";
+
+    if (option === answer) return "bg-green-300! text-shadow-gray-700!";
+
+    if (option === selectedAnswer && option !== answer)
+      return "bg-red-400! text-shadow-gray-700!";
+
+    return "";
+  };
 
   const handleSelected = (option: string) => {
-    setSetselectedAnswer(option);
+    if (!isSubmit) setSetselectedAnswer(option);
   };
 
   return (
     <section
-      className={`flex flex-col space-y-3 rounded-xl p-3 ${isOptionLessThan3Character && "grid grid-cols-2 grid-rows-2 gap-6"}`}
+      className={`flex flex-col space-y-3 rounded-xl p-3 ${isSubmit && "pointer-events-none"} ${isOptionLessThan3Character && "grid grid-cols-2 grid-rows-2 gap-6"}`}
     >
       {question.options.map((option: string, index: number) => (
         <button
           onClick={() => handleSelected(option)}
-          className={`${correctAnswer} ${btnBlockStyle} ${selected(option)} h-full w-full rounded-lg border-0 bg-gray-600 p-3 text-lg font-medium text-white/90 hover:scale-105 hover:cursor-pointer hover:bg-gray-500 hover:text-white`}
+          className={`${getOptionStyle(option)} ${btnBlockStyle} relative h-full w-full rounded-lg border-0 bg-gray-600 p-3 text-lg font-medium text-white/90 hover:scale-105 hover:cursor-pointer hover:bg-gray-500 hover:text-white`}
           key={option}
         >
-          {indexOrder[index]} {option}
+          <span className="mr-2 text-gray-100">{indexOrder[index]}</span>{" "}
+          {option}
         </button>
       ))}
     </section>

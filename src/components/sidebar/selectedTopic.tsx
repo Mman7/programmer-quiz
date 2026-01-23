9fff77b70b474814d49f04b6d62914fea53b847d
Eric Man <youzai0427@gmail.com>
2026-01-23 13:40:19 +0800

    refactor: button component into separate

--- src/components/sidebar/selectedTopic.tsx
+++ src/components/sidebar/selectedTopic.tsx
@@ -1,39 +1,17 @@
 "use client";
 
-import { useQuizOption } from "@/src/store/useSelectedTopicsStore";
 import Topic from "@/src/types/topic";
 import { usePathname } from "next/navigation";
-
-const color: any = {
-  easy: "btn-success",
-  medium: "btn-warning",
-  hard: "btn-error",
-};
+import RemovableTopicBadge from "../topicBadge/removableTopicBadge";
+import TopicBadge from "../topicBadge/topicBadge";
 
 export default function SelectedTopic({ topic }: { topic: Topic }) {
-  const { removeTopic } = useQuizOption();
   const pathname = usePathname();
-  const isQuizPage = pathname == "/quiz";
-
-  const handleRemove = () => {
-    removeTopic(topic);
-  };
+  const isQuizPage = pathname == "/quiz" || pathname.startsWith("/quiz/");
 
   return isQuizPage ? (
-    <button
-      className={`btn btn-outline group transition-all duration-75 ${color[topic.difficulty]}`}
-    >
-      <h1 className="">{topic.name}</h1>
-    </button>
+    <TopicBadge topic={topic} />
   ) : (
-    <button
-      onClick={() => handleRemove()}
-      className={`btn btn-outline group transition-all duration-75 ${color[topic.difficulty]}`}
-    >
-      <h1 className="-mr-3 group-hover:mr-0">{topic.name}</h1>
-      <h1 className="invisible transition-all duration-75! group-hover:visible">
-        X
-      </h1>
-    </button>
+    <RemovableTopicBadge topic={topic} />
   );
 }

7b375ec455b4ed1e25566a05e498843b729857b6
Eric Man <youzai0427@gmail.com>
2026-01-24 06:25:23 +0800

    refactor: remove QuestionWithoutAnswer

--- src/app/quiz/[id]/page.tsx
+++ src/app/quiz/[id]/page.tsx
@@ -4,20 +4,19 @@ import InfiniteLoading from "@/src/components/infiniteLoading";
 import QuestionSection from "@/src/components/quizPage/questionSection";
 import { useMounted } from "@/src/hooks/useMounted";
 import { useQuizGame } from "@/src/store/useQuizGameStore";
-import { QuestionWithoutAnswer, QuizQuestion } from "@/src/types/quizQuestion";
+import { QuizQuestion } from "@/src/types/quizQuestion";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function QuestionPage() {
-  const [question, setQuestion] = useState<
-    QuizQuestion | QuestionWithoutAnswer | undefined
-  >();
+  const [question, setQuestion] = useState<QuizQuestion>();
   const { questions, setLastQuestionIndex } = useQuizGame();
   const mounted = useMounted();
   const params = useParams();
   const id = Number(params.id);
 
   useEffect(() => {
+    // Load question from zustand
     setQuestion(questions[id]);
     setLastQuestionIndex(id);
   }, [mounted]);

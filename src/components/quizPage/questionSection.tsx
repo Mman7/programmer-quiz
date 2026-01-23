16dbc785a91a56935a527419258bfaf68c10bf32
Eric Man <youzai0427@gmail.com>
2026-01-24 06:46:21 +0800

    feat: added submit button functionality

--- src/components/quizPage/questionSection.tsx
+++ src/components/quizPage/questionSection.tsx
@@ -1,9 +1,14 @@
 import { QuizQuestion } from "@/src/types/quizQuestion";
-import { useParams, useRouter } from "next/navigation";
-import { useState } from "react";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import QuestionOptions from "./questionOptions";
 import TopicBadge from "../topicBadge/topicBadge";
 import { Topic } from "@/src/types/topic";
+import { getQuizAnswer } from "@/src/utils/quiz_client/quiz_api";
+import { Answer, RequestAnswer } from "@/src/types/answer";
+import { useQuizGame } from "@/src/store/useQuizGameStore";
+import CheckCorrect from "./showCorrect";
+import NavigationButton from "./navigationButton";
 
 interface QuestionSectionProps {
   question: QuizQuestion;
@@ -12,62 +17,83 @@ interface QuestionSectionProps {
 export default function QuestionSection({ question }: QuestionSectionProps) {
   const [selectedAnswer, setSetselectedAnswer] = useState<string>("");
   const [reason, setReason] = useState<string>("");
-  const [answer, setAnswer] = useState<string>("");
+  const [answer, setAnswer] = useState<string | null>(null);
   const [isSubmit, setIsSubmit] = useState<boolean>(false);
-  const router = useRouter();
+  const { updateQuestionFromArray } = useQuizGame();
   const params = useParams();
   const id = Number(params.id);
+
   const topic: Topic = {
     name: question.topic,
     difficulty: question.difficulty,
   };
-  console.log(question.topic);
-  const submitAnswer = () => {
-    //TODO finish this function
+
+  const submitAnswer = async () => {
+    const request: RequestAnswer = {
+      questionId: question.questionId,
+      topic: { difficulty: question.difficulty, name: question.topic },
+    };
+    const response: Answer = await getQuizAnswer({ req: request }).then(
+      (res) => {
+        return res;
+      },
+    );
+    const newQuestion: QuizQuestion = {
+      ...question,
+      userAnswer: selectedAnswer,
+      reason: response.reason,
+      answer: response.answer,
+    };
+
+    updateQuestionFromArray(newQuestion, id);
+    setAnswer(response.answer);
+    setReason(response.reason);
+    setIsSubmit(true);
   };
 
-  const handleNextQuestion = () => {
-    router.push(`${id + 1}`);
-  };
+  // Load previous data
+  useEffect(() => {
+    if (question.answer) setAnswer(question.answer);
+    if (question.userAnswer) setSetselectedAnswer(question.userAnswer);
+    if (question.userAnswer) setIsSubmit(true);
+    if (question.reason) setReason(question.reason);
+  }, []);
 
   return (
     <div className="m-auto w-full p-3">
       <h1 className="p-2.5 text-center text-3xl font-bold">Quiz Game</h1>
-      <section className="rounded-xl bg-white/50 p-6 backdrop-blur-md">
+      <section className="w-full flex-col rounded-xl bg-white/40 p-6 backdrop-blur-md">
         <h1 className="mb-2 text-2xl font-medium text-shadow-gray-600">
           {id}. {question.questionText}
         </h1>
-        <TopicBadge topic={topic} />
+        <TopicBadge topic={topic} className="max-w-30" />
         <QuestionOptions
+          isSubmit={isSubmit}
           answer={answer}
           question={question}
           selectedAnswer={selectedAnswer}
           setSetselectedAnswer={setSetselectedAnswer}
         />
-        <h1
-          className={`${isSubmit && "visible"} ${answer === selectedAnswer ? "text-green-300" : "text-red-400"} v invisible my-2 rounded-xl bg-white/60 p-4 font-bold`}
+        <CheckCorrect
+          isCorrect={answer === selectedAnswer}
+          isSubmit={isSubmit}
+        />
+        <h2
+          className={`${isSubmit && "block!"} my-2 hidden rounded-lg bg-white/60 p-4 font-medium`}
         >
-          {answer === selectedAnswer
-            ? "Answer is correct 👍"
-            : "Answer is wrong, you almost get it 😁"}
-        </h1>
-        <p className={`${isSubmit && "block"} hidden font-medium`}>
-          Reason:
-          {reason}
-        </p>
+          <p className="text-lg">
+            <span className="mr-2">Reason:</span>
+            {reason}
+          </p>
+        </h2>
         <button
-          onClick={() => submitAnswer()}
-          className="btn btn-info btn-outline mt-3 w-full"
+          onClick={() => (selectedAnswer !== "" ? submitAnswer() : "")}
+          className={`btn ${selectedAnswer !== "" && "btn-info"} ${selectedAnswer === "" && "cursor-not-allowed"} ${isSubmit && "bg-sky-400 text-gray-800"} btn-outline mt-3 w-full`}
         >
           Check Answer!
         </button>
       </section>
-      <button
-        onClick={() => handleNextQuestion()}
-        className={`${isSubmit && "visible"} btn btn-secondary btn-outline invisible mt-3 w-full`}
-      >
-        Next!
-      </button>
+      <NavigationButton isSubmit={isSubmit} />
     </div>
   );
 }

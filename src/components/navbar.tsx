21fecc25b858e87d99d141d3e345577c68c9ac9b
Eric Man <youzai0427@gmail.com>
2026-01-23 13:37:45 +0800

    feat: added continue and restart button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,19 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+"use client";
+
+import {
+  faArrowRotateRight,
+  faBars,
+  faRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ModelName, useConfirmModal } from "../store/useComfirmModalStore";
+import { usePathname, useRouter } from "next/navigation";
+import { useQuizGame } from "../store/useQuizGameStore";
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const isQuizPage = pathname == "/quiz" || pathname.startsWith("/quiz/");
+
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
@@ -15,7 +27,46 @@ export default function Navbar() {
       <div className="navbar-center">
         <h1 className="text-xl font-medium text-black">Programmer Quiz</h1>
       </div>
-      <div className="navbar-end"></div>
+      <div className="navbar-end">
+        {isQuizPage ? <RestartButton /> : <ContinueButton />}
+      </div>
     </div>
+  );
+}
+
+function ContinueButton() {
+  const { questions, lastQuestionIndex } = useQuizGame();
+  const router = useRouter();
+
+  const handleContinueGame = () => {
+    router.push(`/quiz/${lastQuestionIndex}`);
+  };
+  return (
+    questions.length > 1 && (
+      <button
+        onClick={() => handleContinueGame()}
+        className="btn btn-success group"
+      >
+        <FontAwesomeIcon
+          className="sm:opacity-0 group-hover:sm:opacity-100"
+          icon={faRightToBracket}
+        />
+        <h1 className="-ml-5 hidden group-hover:m-0 sm:block">Continue</h1>
+      </button>
+    )
+  );
+}
+
+function RestartButton() {
+  const { setModalName } = useConfirmModal();
+
+  const handleRestartGame = () => {
+    setModalName(ModelName.restartModal);
+  };
+  return (
+    <button onClick={() => handleRestartGame()} className="btn btn-secondary">
+      <FontAwesomeIcon className="sm:hidden" icon={faArrowRotateRight} />
+      <h1 className="hidden sm:block">Restart Game</h1>
+    </button>
   );
 }

1babc37b516f3cdd5e244b9157d732d70933ada3
Eric Man <youzai0427@gmail.com>
2026-01-23 13:50:52 +0800

    feat : added confirm modal

---
+++ src/components/modal/comfirmModal.tsx
@@ -0,0 +1,53 @@
+import { ModelName, useConfirmModal } from "@/src/store/useComfirmModalStore";
+
+interface ComfirmModalProps {
+  title: string;
+  description: string;
+  callback: () => void;
+  modalName: ModelName;
+}
+
+export default function ComfirmModal({
+  callback,
+  description,
+  modalName,
+  title,
+}: ComfirmModalProps) {
+  const { closeModal, currentShowingModalName } = useConfirmModal();
+
+  const handleRestart = () => {
+    callback();
+    closeModal();
+  };
+
+  return (
+    <dialog
+      open={currentShowingModalName === modalName}
+      id="my_modal_3"
+      className="modal"
+    >
+      <div className="modal-box">
+        <h3 className="mb-4 text-lg font-bold">{title}</h3>
+        <p>{description}</p>
+
+        <form method="dialog">
+          {/*  close the modal */}
+          <button
+            onClick={() => closeModal()}
+            className="btn btn-sm btn-circle btn-ghost absolute top-2 right-2"
+          >
+            ✕
+          </button>
+        </form>
+        <section className="mt-3 flex justify-end gap-3 p-3">
+          <button className="btn" onClick={() => closeModal()}>
+            Cancel
+          </button>
+          <button className="btn btn-secondary" onClick={() => handleRestart()}>
+            Confirm
+          </button>
+        </section>
+      </div>
+    </dialog>
+  );
+}

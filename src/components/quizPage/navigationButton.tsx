1de0be3ce769c1790a2401f670f982353d56e290
Eric Man <youzai0427@gmail.com>
2026-01-24 20:39:16 +0800

    fix: navigation button showing on last question

--- src/components/quizPage/navigationButton.tsx
+++ src/components/quizPage/navigationButton.tsx
@@ -1,9 +1,11 @@
+import { useQuizGame } from "@/src/store/useQuizGameStore";
 import { useParams, useRouter } from "next/navigation";
 
 export default function NavigationButton({ isSubmit }: { isSubmit: boolean }) {
   const router = useRouter();
   const params = useParams();
   const id = Number(params.id);
+  const { questions } = useQuizGame();
 
   const handleNextQuestion = () => {
     router.push(`${id + 1}`);
@@ -14,16 +16,16 @@ export default function NavigationButton({ isSubmit }: { isSubmit: boolean }) {
   };
 
   return (
-    <div className="flex">
+    <div className="flex gap-3">
       <button
         onClick={() => handleGoBack()}
-        className={`${isSubmit && "visible"} ${id === 1 && "hidden"} btn btn-secondary btn-outline invisible m-3 flex-1 backdrop-blur-2xl`}
+        className={`${isSubmit && "visible"} ${id === 1 && "hidden"} btn btn-secondary btn-outline invisible flex-1 backdrop-blur-2xl`}
       >
         Go back!
       </button>
       <button
         onClick={() => handleNextQuestion()}
-        className={`${isSubmit && "visible"} btn btn-secondary btn-outline invisible m-3 flex-1 backdrop-blur-2xl`}
+        className={`${isSubmit && "visible"} ${id === questions.length && "hidden"} btn btn-secondary btn-outline invisible flex-1 backdrop-blur-2xl`}
       >
         Next!
       </button>

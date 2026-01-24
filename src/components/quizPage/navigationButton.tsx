fe4722344ad6739205bc386cc87391aa3b1f7e1d
Eric Man <youzai0427@gmail.com>
2026-01-24 20:24:06 +0800

    style: added blur background for button

--- src/components/quizPage/navigationButton.tsx
+++ src/components/quizPage/navigationButton.tsx
@@ -4,23 +4,26 @@ export default function NavigationButton({ isSubmit }: { isSubmit: boolean }) {
   const router = useRouter();
   const params = useParams();
   const id = Number(params.id);
+
   const handleNextQuestion = () => {
     router.push(`${id + 1}`);
   };
+
   const handleGoBack = () => {
     router.push(`${id - 1}`);
   };
+
   return (
     <div className="flex">
       <button
         onClick={() => handleGoBack()}
-        className={`${isSubmit && "visible"} ${id === 1 && "hidden"} btn btn-secondary btn-outline invisible m-3 flex-1`}
+        className={`${isSubmit && "visible"} ${id === 1 && "hidden"} btn btn-secondary btn-outline invisible m-3 flex-1 backdrop-blur-2xl`}
       >
         Go back!
       </button>
       <button
         onClick={() => handleNextQuestion()}
-        className={`${isSubmit && "visible"} btn btn-secondary btn-outline invisible m-3 flex-1`}
+        className={`${isSubmit && "visible"} btn btn-secondary btn-outline invisible m-3 flex-1 backdrop-blur-2xl`}
       >
         Next!
       </button>

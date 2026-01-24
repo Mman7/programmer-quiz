9a22a87ffcd1d00cb6725b455e745cdb02ca9615
Eric Man <youzai0427@gmail.com>
2026-01-24 20:23:45 +0800

    style: remove padding in grid button

--- src/components/quizPage/questionOptions.tsx
+++ src/components/quizPage/questionOptions.tsx
@@ -18,7 +18,7 @@ export default function QuestionOptions({
   setSetselectedAnswer,
 }: QuestionOptionsProps) {
   const isOptionLessThan3Character: boolean = question.options[0].length < 3;
-  const btnBlockStyle = isOptionLessThan3Character && "p-6";
+  const btnBlockStyle = isOptionLessThan3Character;
   const getOptionStyle = (option: string) => {
     if (!isSubmit)
       return option === selectedAnswer ? "bg-info! text-white! scale-105" : "";
@@ -37,12 +37,12 @@ export default function QuestionOptions({
 
   return (
     <section
-      className={`flex flex-col space-y-3 rounded-xl p-3 ${isSubmit && "pointer-events-none"} ${isOptionLessThan3Character && "grid grid-cols-2 grid-rows-2 gap-6"}`}
+      className={`flex flex-col space-y-3 rounded-xl p-4 ${isSubmit && "pointer-events-none"} ${isOptionLessThan3Character && "grid grid-cols-2 grid-rows-2 gap-6"}`}
     >
       {question.options.map((option: string, index: number) => (
         <button
           onClick={() => handleSelected(option)}
-          className={`${getOptionStyle(option)} ${btnBlockStyle} relative h-full w-full rounded-lg border-0 bg-gray-600 p-2 text-lg font-medium text-white/90 hover:scale-105 hover:cursor-pointer hover:bg-gray-500 hover:text-white`}
+          className={`${getOptionStyle(option)} ${btnBlockStyle} relative h-full w-full rounded-lg border-0 bg-gray-600 p-2.5 text-lg font-medium text-white/90 hover:scale-105 hover:cursor-pointer hover:bg-gray-500 hover:text-white`}
           key={option}
         >
           <span className="mr-2 text-gray-100">{indexOrder[index]}</span>{" "}

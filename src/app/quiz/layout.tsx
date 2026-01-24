9f562069af9a720f156bc41592584af29f045602
Eric Man <youzai0427@gmail.com>
2026-01-25 03:36:05 +0800

    fix: position bug

--- src/app/quiz/layout.tsx
+++ src/app/quiz/layout.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import GoBackButton from "@/src/components/goBackButton";
-import { faBackward } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRouter } from "next/navigation";
 
 export default function QuizPageLayout({
   children,
@@ -12,8 +9,8 @@ export default function QuizPageLayout({
 }) {
   return (
     <>
-      <div className="flex h-[calc(100vh-4rem)]">
-        <GoBackButton />
+      <div className="flex h-[calc(100vh-4rem)] flex-col">
+        <GoBackButton className="absolute" />
         {children}
       </div>
     </>

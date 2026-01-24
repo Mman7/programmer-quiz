12de4e4da14ee4b38cd8e0964b838aa7a341e006
Eric Man <youzai0427@gmail.com>
2026-01-24 12:20:36 +0800

    docs: add documentation and cleanup unused code

--- src/types/answer.d.ts
+++ src/types/answer.d.ts
@@ -1,9 +1,11 @@
+// Represents a user's answer to a question
 export type Answer = {
   questionId: string;
   answer: string;
   reason: string;
 };
 
+// Represents a request for an answer to a question
 export type RequestAnswer = {
   questionId: string;
   topic: Topic;

8dad6fe4528e6211665f0a47d12107106ff8d318
Eric Man <youzai0427@gmail.com>
2026-01-24 16:22:21 +0800

    style: added logo center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 "use client";
-
+import Image from "next/image";
 import Searchbar from "../components/searchbar/searchbar";
 import StartQuizButton from "../components/startQuizButton";
 
 export default function Home() {
   return (
     <div className="m-auto w-3/4 max-w-2xl">
+      <div className="m-auto mb-8 size-64 rounded-4xl bg-white">
+        <Image
+          className="m-auto"
+          src="/img/logo.png"
+          width={300}
+          height={300}
+          alt="Logo"
+        />
+      </div>
       <Searchbar />
       <StartQuizButton />
     </div>

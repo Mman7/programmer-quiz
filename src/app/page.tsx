51df1844bc6eb7a5dbe0ba0c8ad94d9613f93607
Eric Man <youzai0427@gmail.com>
2026-01-25 12:46:12 +0800

    style: fix padding inconsistent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import StartQuizButton from "../components/startQuizButton";
 
 export default function Home() {
   return (
-    <div className="m-auto w-3/4 max-w-2xl">
-      <div className="m-auto mb-8 size-64 rounded-4xl bg-white/10 backdrop-blur-2xl">
+    <div className="m-auto flex h-full w-3/4 max-w-2xl flex-col justify-center">
+      <div className="mx-auto mb-8 size-64 rounded-4xl bg-white/10 backdrop-blur-2xl">
         <Image
           className="m-auto"
           src="/img/logo.png"

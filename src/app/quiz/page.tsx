a848b111ad5c8b9f32c39e6ccd7818a58467c6bb
Eric Man <youzai0427@gmail.com>
2026-01-27 04:24:07 +0800

    feat: added waiting truck

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -5,6 +5,7 @@ import { useQuizOption } from "@/src/store/useSelectedTopicsStore";
 import { fetchQuiz, Quiz } from "@/src/utils/quiz_client/quiz_api";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function QuizPage() {
   const { numberOfQuiz, topics } = useQuizOption();
@@ -28,9 +29,15 @@ export default function QuizPage() {
 
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <h1 className="m-auto rounded-2xl bg-white/40 p-8 backdrop-blur-2xl sm:text-3xl">
-        Cooking up a question for you
-        <span className="loading loading-dots sm:loading-xl loading-sm ml-4"></span>
+      <h1 className="m-auto rounded-2xl bg-white/10 p-8 font-bold backdrop-blur-xl sm:text-3xl">
+        <DotLottieReact
+          className="w-100"
+          src="/lottie/truck_delivering.lottie"
+          loop
+          autoplay
+        />
+        <span className="text-white">Delivering question for you</span>
+        <span className="loading loading-dots sm:loading-xl loading-sm ml-4 text-white"></span>
       </h1>
     </div>
   );

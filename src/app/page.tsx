6c087e326c71f694a6bdb51571214f9f913c6d67
Eric Man <youzai0427@gmail.com>
2026-01-30 14:52:29 +0800

    fix: logo too big for small mobile screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,13 @@ import StartQuizButton from "../components/startQuizButton";
 
 export default function Home() {
   return (
-    <div className="m-auto flex h-full w-3/4 max-w-2xl flex-col justify-center">
-      <div className="mx-auto mb-8 size-64 rounded-4xl bg-white/10 shadow-2xl backdrop-blur-2xl">
+    <div className="m-auto flex h-full max-w-2xl flex-col justify-center">
+      <div className="mx-auto mb-8 aspect-square w-full max-w-64 rounded-4xl bg-white/10 shadow-2xl backdrop-blur-2xl">
         <Image
           loading="eager"
           className="m-auto"
           src="/img/logo.png"
-          width={300}
-          height={300}
+          fill={true}
           alt="Logo"
         />
       </div>
